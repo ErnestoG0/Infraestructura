50903f84334c8d2d23543721f38bdf6b040f06c7
themagicalturtle <96089369+themagicalturtle@users.noreply.github.com>
2021-12-13 17:35:09 -0700

    client.php conexion

---
+++ funciones/client.php
@@ -0,0 +1,12 @@
+<php?
+//validamos
+$user = "root";
+$pass = "";
+$host = "localhost";
+//conexion
+$conection = mysql_connection ($host, $user, $pass);
+
+$nombre = $_POST["NameClient"];
+$RFC = $_POST[];
+
+?>
